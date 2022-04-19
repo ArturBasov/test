fee26ba4a7ad2e90fe1c7daeaf9745ee8ac92a43
ArturBasov <proboskis@gmail.com>
2022-04-20 00:53:18 +0300

    add writeYourGenres() and showMyDB()

--- js/script.js
+++ js/script.js
@@ -1,21 +1,77 @@
 "use strict";
 
-const numberOfFilms = +prompt("How much films have you watched yet?", "");
+let numberOfFilms;
+
+function start() {
+  while (true) {
+    numberOfFilms = +prompt("How much films have you watched yet?", "");
+    if (
+      typeof numberOfFilms == "number" &&
+      numberOfFilms >= 0 &&
+      numberOfFilms != ""
+    ) {
+      break;
+    } else {
+      alert("Wrong answer!");
+    }
+  }
+  detectPersonalLevel();
+}
+
+function detectPersonalLevel() {
+  if (numberOfFilms < 10) {
+    alert("So little...");
+  } else if (numberOfFilms < 30) {
+    alert("Not bad!");
+  } else {
+    alert("OMG!!!");
+  }
+}
+
+start();
 
 const personMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
-  privat: false,
+  privat: true,
 };
 console.log(personMovieDB);
 
-for (let i = 0; i < 2; i++) {
-  let nameOfTheLastFilm = prompt("One of the last movies you watched", "");
-  let rateOfTheLastFilm = +prompt("Rate it");
-  personMovieDB.movies[nameOfTheLastFilm] = rateOfTheLastFilm;
-  console.log(personMovieDB.movies);
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    let nameOfTheLastFilm;
+    let rateOfTheLastFilm;
+    while (true) {
+      nameOfTheLastFilm = prompt("One of the last movies you watched", "");
+      if (!nameOfTheLastFilm || nameOfTheLastFilm.length >= 50) {
+        alert("Wrong answer!");
+      } else {
+        break;
+      }
+    }
+    rateOfTheLastFilm = +prompt("Rate it");
+    personMovieDB.movies[nameOfTheLastFilm] = rateOfTheLastFilm;
+    console.log(personMovieDB.movies);
+  }
 }
 
-alert(JSON.stringify(personMovieDB));
+rememberMyFilms();
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personMovieDB.genres[i - 1] = prompt(
+      `What's your favourite movie genre?\n Top 3. №${i} - ?`
+    );
+  }
+}
+writeYourGenres();
+
+function showMyDB() {
+  if (personMovieDB.privat == false) {
+    alert(JSON.stringify(personMovieDB));
+  }
+}
+
+showMyDB();
